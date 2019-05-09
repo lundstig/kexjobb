ca579a1ea5801c3ca091876ef71c127a859457bf
Karl Lundstig <karl.lundstig@gmail.com>
2019-05-09 21:43:29 +0200

    Log to file as well

--- src/main.py
+++ src/main.py
@@ -4,6 +4,9 @@ import data
 import export
 
 coloredlogs.install(fmt="[%(asctime)s] %(message)s", level=logging.INFO)
+fh = logging.FileHandler('log')
+fh.setLevel(logging.DEBUG)
+logging.getLogger().addHandler(fh)
 
 subjects = data.load_subjects()
 analysis.print_offset_stats(subjects)
